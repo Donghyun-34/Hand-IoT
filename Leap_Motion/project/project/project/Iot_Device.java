0d69a5ed6adc92bb98c2226196c07673a07d665f
4647sjs <4647sjs@naver.com>
2021-04-16 14:14:37 +0900

    4/16일 정적제스처 추가

---
+++ Leap_Motion/project/project/project/Iot_Device.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
+public class Iot_Device {
+     public static String de_name; //sliding_window
+     public static int de_addr; //192.168.121.1
+     public static byte sta_gesture; //six code 101101
+     public static int Dyn_gesture_1; // swipe left->right
+     public static int Dyn_gesture_2; // swipe right->left
+     public static int Dyn_gesture_3; // circle_clock_wise
+     public static int Dyn_gesture_4; // circle_Anti_clock_wise
+
+}
