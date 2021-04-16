4f41db21429d943201b7cb8b6c66c1fc4d69748d
Donghyun-34 <53668828+Donghyun-34@users.noreply.github.com>
2021-04-16 14:18:12 +0900

    Delete Iot_Device.java

--- Leap_Motion/project/project/Iot_Device.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.*;
-
-public class Iot_Device {
-     public static String de_name;
-     public static int de_addr;
-     public static int sta_gesture; //six code 101101
-}
